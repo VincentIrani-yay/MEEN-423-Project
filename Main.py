22a1df8be1a61972fac63972170fa4ac8f58466b
VincentIrani <vtirani@gmail.com>
2024-04-17 18:34:20 -0500

    werwerwer

--- Main.py
+++ Main.py
@@ -1,3 +1,15 @@
-#Testing Testing 123
-print('Hello')
-#It works
+#MEEN-423 Final Project
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.optimizers import Adam
+
+from sklearn.metrics import mean_squared_error, r2_score
