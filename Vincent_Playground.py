d3bf3537879ff532039a4dc26530747f6163eabe
VincentIrani <vtirani@gmail.com>
2024-04-28 14:43:54 -0500

    werwerwer

--- Vincent_Playground.py
+++ Vincent_Playground.py
@@ -9,6 +9,20 @@ from sklearn.linear_model import LinearRegression
 
 # Load data from the CSV file
 df = pd.read_csv("star_classification.csv")
-# Convert categories to numbers
+
+# Convert categories to numbers and splitting into features and response
 df["class"]=[0 if i == "GALAXY" else 1 if i == "STAR" else 2 for i in df["class"]]
-print(df["class"].value_counts())
+
+features = df.loc[:, df.columns != "class"]
+response = df['class']
+
+# Splitting test and training data
+x_train, x_test, y_train, y_test = train_test_split(features, response, test_size=0.2, random_state = 53)
+
+# Scaling the data
+scaler = StandardScaler()
+scaler.fit(x_train)
+x_train_sc = scaler.transform(x_train)
+x_test_sc = scaler.transform(x_test)
+
+
