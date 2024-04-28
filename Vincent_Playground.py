4d0eaf9b1095dad2990f444c9d6d738a97bad283
VincentIrani <vtirani@gmail.com>
2024-04-28 14:17:34 -0500

    Added the star CSV file and a playground for me so we can code at the same time

---
+++ Vincent_Playground.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split, KFold, cross_val_score
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LinearRegression
+
+# Load data from the CSV file
+df = pd.read_csv("star_classification.csv")
+# Convert categories to numbers
+df["class"]=[0 if i == "GALAXY" else 1 if i == "STAR" else 2 for i in df["class"]]
+print(df["class"].value_counts())
