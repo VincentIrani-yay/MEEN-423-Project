8bba2ab17d3526cb5de0e406a766a6febabc2aac
Charles Beare <155017983+VeniVidiVici27@users.noreply.github.com>
2024-04-28 13:37:15 -0500

    new dataset

--- Main.py
+++ Main.py
@@ -1,21 +1,15 @@
 
-import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.svm import SVR
-from sklearn.metrics import mean_squared_error, r2_score
+import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split, KFold, cross_val_score
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LinearRegression
 
 # Load data from the CSV file
-data = pd.read_csv('Concrete_Data.csv')
-
-# Define the feature columns and target variable
-feature_columns = ['Cement (component 1)(kg in a m^3 mixture)', 'Blast Furnace Slag (component 2)(kg in a m^3 mixture)', 'Fly Ash (component 3)(kg in a m^3 mixture)', 'Water  (component 4)(kg in a m^3 mixture)', 'Superplasticizer (component 5)(kg in a m^3 mixture)', 'Coarse Aggregate  (component 6)(kg in a m^3 mixture)', 'Fine Aggregate (component 7)(kg in a m^3 mixture)', 'Age (day)']
-target_column = 'Concrete Compressive Strength MPA'
-
-# Select features and target from the dataset
-X = data[feature_columns]  # Features (input)
-y = data[target_column]    # Target variable (output)
-
-# Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+df = pd.read_csv("star_classification.csv")
+# Convert categories to numbers
+df["class"]=[0 if i == "GALAXY" else 1 if i == "STAR" else 2 for i in df["class"]]
+print(df["class"].value_counts())
